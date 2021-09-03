80f23099c81df2c887489d5853924eff34eb3f7d
Alex Macon <alexdmacon@gmail.com>
2021-09-02 23:30:23 -0500

    Can now retrieve inputs from local storage upon page load. Don't quite know how to put the little lock icon on the buttons.

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,11 @@
 // https://getbootstrap.com/docs/4.0/layout/grid/
 // https://jqueryui.com/
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit
-
-
+// https://stackoverflow.com/questions/7892446/how-to-save-to-localstorage-from-textarea
+// https://stackoverflow.com/questions/34767630/typeerror-addeventlistener-is-not-a-function
+// https://stackoverflow.com/questions/42963091/how-to-save-the-value-of-textarea-to-localstorage-then-display-it-in-the-same-te
+// https://momentjs.com/
+// https://stackoverflow.com/questions/3842614/how-do-i-call-a-javascript-function-on-page-load
 
 
 var currentDayEl = $('#currentDay');
@@ -52,52 +55,36 @@ function colorCodeBlocks(){
 }
 colorCodeBlocks()
 
-
 // Clicking save buttons calls saveSchedule function
 saveBtn.on("click", saveSchedule);
 
+
 function saveSchedule(){
-// Traverses the DOM by going from the clicked button (aka "this") to its siblings, grabbing the input value from the textarea sibling with the class of "description"
-    var inputValue = $(this).siblings(".description").val();
-// Traverses the DOM by going from the clicked button (aka "this") to its parent div element and grabbing that DIV's HTML-assigned ID.
+
+// Traverses the DOM by going from the clicked button (aka "this") to its siblings, grabbing the input value from the textarea sibling element
+    var inputValue = $(this).siblings("textarea").val();
+
+// Traverses the DOM by going from the clicked button (aka "this") to its parent div element (the row) and grabbing that div's HTML-assigned ID.
     var timeBlockID = $(this).parent().attr("id");
-// Saves the above to localStorage as key and value, respectively
-    localStorage.setItem(inputValue, timeBlockID);
+
+// Saves the above variables to localStorage as corresponding key and value, respectively
+    localStorage.setItem(timeBlockID, inputValue);
 }
 
+// uses jquery and DOM to select individual textarea elements by id, retrieves data stored in local storage associated with corresponding key, puts it as the value in respective textarea element. If no data stored, no value will be pulled.
+function retrieveSchedule(){
+    $("#15 textarea").val(localStorage.getItem("15"));
+    $("#16 textarea").val(localStorage.getItem("16"));
+    $("#17 textarea").val(localStorage.getItem("17"));
+    $("#18 textarea").val(localStorage.getItem("18"));
+    $("#19 textarea").val(localStorage.getItem("19"));
+    $("#20 textarea").val(localStorage.getItem("20"));
+    $("#21 textarea").val(localStorage.getItem("21"));
+    $("#22 textarea").val(localStorage.getItem("22"));
+    $("#23 textarea").val(localStorage.getItem("23"));
+}
 
-
-
+retrieveSchedule();
 
 
 console.log(currentHour)
-
-
-
-
-
-
-/* ## User Story
-
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist */
