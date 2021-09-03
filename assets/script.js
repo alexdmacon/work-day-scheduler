cd9310502db095d144a73879fd19264f46f84aec
Alex Macon <alexdmacon@gmail.com>
2021-09-03 14:56:35 -0500

    added lock icon to save buttons

--- assets/script.js
+++ assets/script.js
@@ -27,37 +27,37 @@ displayDate();
 // Change text block color depending on time of day
 function colorCodeBlocks(){
 
-// Call this function to each individual time block div
+// Here comes a method that will run on each individual time block div
     timeBlockEls.each(function () {
 
-// parse each individual time block and turn its HTML-defined id (a number corresponding to a military-time hour, in this case) into an integer that we can subsequently refer to using timeBlockHour
+// parse each individual time block div (aka "this") and turn its HTML-defined id attribute (a number corresponding to a military-time hour, in this case) into an integer that we can subsequently refer to using the newly defined variable timeBlockHour
         var timeBlockHour = parseInt($(this).attr("id"));
         console.log(timeBlockHour);
 
-// compare each individual time block id to the current hour and apply or remove the corresponding background color class to said text block
+// compare each individual time block id to the current hour and apply or remove the corresponding background color class to said text block (aka "this")
         if (timeBlockHour < currentHour) {
+            $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
-            $(this).addClass("past");
         }
         else if (timeBlockHour == currentHour) {
+            $(this).addClass("present");
             $(this).removeClass("past");
             $(this).removeClass("future");
-            $(this).addClass("present");
         }
         else {
+            $(this).addClass("future");
             $(this).removeClass("present");
             $(this).removeClass("past");
-            $(this).addClass("future");
     }
     }
     )
 }
+
 colorCodeBlocks()
 
 // Clicking save buttons calls saveSchedule function
 saveBtn.on("click", saveSchedule);
-
 
 function saveSchedule(){
 
@@ -69,9 +69,9 @@ function saveSchedule(){
 
 // Saves the above variables to localStorage as corresponding key and value, respectively
     localStorage.setItem(timeBlockID, inputValue);
-}
+};
 
-// uses jquery and DOM to select individual textarea elements by id, retrieves data stored in local storage associated with corresponding key, puts it as the value in respective textarea element. If no data stored, no value will be pulled.
+// uses jquery and DOM to select individual textarea elements by id, retrieves data stored in local storage associated with corresponding key, then puts it as the value in respective textarea element. If no data stored, no value will be pulled.
 function retrieveSchedule(){
     $("#15 textarea").val(localStorage.getItem("15"));
     $("#16 textarea").val(localStorage.getItem("16"));
@@ -87,4 +87,4 @@ function retrieveSchedule(){
 retrieveSchedule();
 
 
-console.log(currentHour)
+console.log(currentHour);
