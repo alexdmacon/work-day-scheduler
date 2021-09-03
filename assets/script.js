b5b3e1f00d8ba91c0f6da3e51e696cc3ace4dc74
Alex Macon <alexdmacon@gmail.com>
2021-09-02 22:53:42 -0500

    have function that saves inputs to local storage

--- assets/script.js
+++ assets/script.js
@@ -15,23 +15,23 @@ var saveBtn = $('.saveBtn');
 var row = $('row');
 
 
+// Display today's date at the top of the page.
 function displayDate() {
     currentDayEl.text(date);
 }
 displayDate();
 
-
 // Change text block color depending on time of day
 function colorCodeBlocks(){
 
-// Apply this function to each individual time block div
+// Call this function to each individual time block div
     timeBlockEls.each(function () {
 
 // parse each individual time block and turn its HTML-defined id (a number corresponding to a military-time hour, in this case) into an integer that we can subsequently refer to using timeBlockHour
         var timeBlockHour = parseInt($(this).attr("id"));
         console.log(timeBlockHour);
 
-// compare the returned integer to the current hour and apply or remove the corresponding background color classes
+// compare each individual time block id to the current hour and apply or remove the corresponding background color class to said text block
         if (timeBlockHour < currentHour) {
             $(this).removeClass("future");
             $(this).removeClass("present");
@@ -52,10 +52,25 @@ function colorCodeBlocks(){
 }
 colorCodeBlocks()
 
+
+// Clicking save buttons calls saveSchedule function
+saveBtn.on("click", saveSchedule);
+
+function saveSchedule(){
+// Traverses the DOM by going from the clicked button (aka "this") to its siblings, grabbing the input value from the textarea sibling with the class of "description"
+    var inputValue = $(this).siblings(".description").val();
+// Traverses the DOM by going from the clicked button (aka "this") to its parent div element and grabbing that DIV's HTML-assigned ID.
+    var timeBlockID = $(this).parent().attr("id");
+// Saves the above to localStorage as key and value, respectively
+    localStorage.setItem(inputValue, timeBlockID);
+}
+
+
+
+
+
+
 console.log(currentHour)
-
-textAreaEls.text.currentHour;
-
 
 
 
